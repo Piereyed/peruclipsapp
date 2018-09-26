a314ed84cc1712d84cb2757fc2c176e791caf7fd
Pier <piereyed@gmail.com>
2018-09-26 12:26:52 -0500

    primera version

---
+++ www/js/index.js
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function tagItemTemplate(id,name, enname) {
+    return (
+        `<div class="panel panel-default">
+<div class="panel-heading">
+<h3 class="panel-title">${name}</h3>
+</div>
+<div class="panel-body">
+Id: ${id}
+Codigo: ${enname}
+</div>
+</div>`
+    )
+}
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        //        var parentElement = document.getElementById(id);
+        //        var listeningElement = parentElement.querySelector('.listening');
+        //        var receivedElement = parentElement.querySelector('.received');
+
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
+
+        // console.log('Received Event: ' + id);
+
+        fetch('https://peruclips.com/api/tags')
+            .then(function(response) {
+            return response.json();
+        })
+            .then(function(myJson) {
+//            console.log(myJson);
+
+            for (var i = 0; i < myJson.length; i++){
+                var obj = myJson[i];                
+                $(".relleno").append(tagItemTemplate(obj['id'],obj['name'], obj['en_name']));
+
+            }
+
+
+        });
+    }
+};
