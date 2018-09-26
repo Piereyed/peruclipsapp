4878d9efe0aa22a430b9396c869b82b792577f58
Pier <piereyed@gmail.com>
2018-09-26 18:04:28 -0500

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -49,8 +49,8 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
-                alert(window.QRScanner); 
-           
+        //                alert(window.QRScanner); 
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -98,24 +98,62 @@ var app = {
         //
         //        });
 
-//        function displayContents(err, text){
-//            if(err){
-//                $("#error").html("Hubo un error al intentar leer el QR CODE");
-//                navigator.notification.alert("Hubo un error al intentar leer el QR CODE");
-//                // an error occurred, or the scan was canceled (error code `6`)
-//            } else {
-//                // The scan completed, display the contents of the QR code:
-//                $("#error").html("The scan completed");
-//                alert(text);
-//            }
-//        }
+        //        function displayContents(err, text){
+        //            if(err){
+        //                $("#error").html("Hubo un error al intentar leer el QR CODE");
+        //                navigator.notification.alert("Hubo un error al intentar leer el QR CODE");
+        //                // an error occurred, or the scan was canceled (error code `6`)
+        //            } else {
+        //                // The scan completed, display the contents of the QR code:
+        //                $("#error").html("The scan completed");
+        //                alert(text);
+        //            }
+        //        }
 
         document.getElementById("qrcode").addEventListener('click', function(){
 
 
             if(window.QRScanner){
 
-                navigator.vibrate(3000);
+                window.QRScanner.prepare(onDone); // show the prompt
+
+                function onDone(err, status){
+                    if (err) {
+                        // here we can handle errors and clean up any loose ends.
+                        $("#error").html(err);
+                    }
+                    if (status.authorized) {
+                        // W00t, you have camera access and the scanner is initialized.
+                        // QRscanner.show() should feel very fast.
+                    } else if (status.denied) {
+                        // The video preview will remain black, and scanning is disabled. We can
+                        // try to ask the user to change their mind, but we'll have to send them
+                        // to their device settings with `QRScanner.openSettings()`.
+                        $("#error").html("denied");
+                    } else {
+                        // we didn't get permission, but we didn't get permanently denied. (On
+                        // Android, a denial isn't permanent unless the user checks the "Don't
+                        // ask again" box.) We can ask again at the next relevant opportunity.
+                    }
+                }
+
+                // Start a scan. Scanning will continue until something is detected or
+                // `QRScanner.cancelScan()` is called.
+                window.QRScanner.scan(displayContents);
+
+                function displayContents(err, text){
+                    if(err){
+                        // an error occurred, or the scan was canceled (error code `6`)
+                    } else {
+                        // The scan completed, display the contents of the QR code:
+                        alert(text);
+                    }
+                }
+
+                // Make the webview transparent so the video preview is visible behind it.
+                window.QRScanner.show();
+                // Be sure to make any opaque HTML elements transparent here to avoid
+                // covering the video.
             }
             else{
                 $("#error").html("No se encontro el lector de qr");
